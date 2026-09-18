430b0a30e558f2e9de0134fb12e54b2baaa62f45
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-18 21:55:24 +0330

    Adding mroe integers

--- Session_02/main.c
+++ Session_02/main.c
@@ -5,7 +5,13 @@ int main(){
     // variable = A reusable container for a value. Behaves as if it were the value it contains.
 
     int age = 21;
-    printf("You are %d years old.", age);
+    printf("You are %d years old\n.", age);
+
+    int year = 2026;
+    printf("The year is %d.\n", year);
+
+    int quantity = 7;
+    printf("You ordered %d computers from our store!");
 
 
     return 0;
