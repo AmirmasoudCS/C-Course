f444d2f4aa7c363b2a45eec91d0b202dea772e74
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-18 21:52:49 +0330

    Defining and printing an int variable

---
+++ Session_02/main.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+
+    // variable = A reusable container for a value. Behaves as if it were the value it contains.
+
+    int age = 21;
+    printf("You are %d years old.", age);
+
+
+    return 0;
+}
