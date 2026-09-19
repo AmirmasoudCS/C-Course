6a5da4e687fff444c478574827f8afbdfbeb68ff
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-19 09:05:50 +0330

    Printing the variables

--- Session_03/main.c
+++ Session_03/main.c
@@ -8,5 +8,16 @@ int main(){
     //                      and optional modifiers (width, precision, flags).
     //                      They control how data is displayed or interpreted.
 
+    int age = 21;                   // %d
+    float price = 19.99;            // %f
+    double pi = 3.1415926535;       // %lf
+    char currency = '$';            // %c
+    char name[] = "Amirmasoud";     // %s
+
+
+
+    printf("age:\t\t%d\nprice:\t\t%f\npi:\t\t\t%.14lf\ncurrency:\t%c\nname:\t\t%s\n", age, price, pi, currency, name);
+
+
     return 0;
 }
