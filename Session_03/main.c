8bd66f1f870b34ffb642bb8d8ee2042fe913c9f1
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-19 09:01:16 +0330

    Setting up the session skeleton

---
+++ Session_03/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    // Format Specifiers = Special tokens that begin with a `%` symbol,    
+    //                      followed by a `character` that specifies the data type
+    //                      and optional modifiers (width, precision, flags).
+    //                      They control how data is displayed or interpreted.
+
+    return 0;
+}
