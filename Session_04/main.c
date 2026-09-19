56a97b5d213e7802e6af3ac0120830223aad7474
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-19 09:23:27 +0330

    Arithmetic operations completed

--- Session_04/main.c
+++ Session_04/main.c
@@ -5,8 +5,28 @@ int main(){
 
     // arithmetic operators =    `+`    `-`     `*`     `/`      `%`     `++`    `--`
 
+    int x = 2, y = 3, z = 0;
 
+    z = x + y;
+    printf("x + y = %d\n", z);
 
+    z = x - y;
+    printf("x - y = %d\n", z);
+
+    z = x * y;
+    printf("x * y = %d\n", z);
+
+    z = x / y;
+    printf("x / y = %d\n", z);
+
+    z = x % y;
+    printf("x %% y = %d\n", z);
+
+    z = ++x;
+    printf("++x = %d\n", z);
+
+    z = --x;
+    printf("--x = %d\n", z);
 
     return 0;
 }
