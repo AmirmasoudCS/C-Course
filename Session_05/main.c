d7a426024ad93548a2686f283636a2a67d1a146b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-19 09:39:19 +0330

    Setting up our session skeleton

---
+++ Session_05/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+    float gpa;
+    char grade;
+    // char name[30];      // Since we are not declaring the value of `name` right away, we need to declare its size. In this case it is `30`.
+
+    // Using variables that are not declared can lead to undefined behavior.
+    // So it is good practice to assign default variables when declaring them.
+
+    age = 0;
+    gpa = 0.0f;
+    grade = '\0';           // Null Terminator : clears out whatever was stored inside our `grade` variable.
+    char name[30] = "";     // An empty string that still  has 30 bytes as space.
+
+
+
+    return 0;
+}
