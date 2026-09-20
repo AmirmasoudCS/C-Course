1492b221d0727524eeb9edef8677d78315b7cba8
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-20 20:55:23 +0330

    Implementing the code for the logic

--- Session_06/main.c
+++ Session_06/main.c
@@ -1,7 +1,25 @@
+// Shopping Cart project
+
+
 #include <stdio.h>
 #include <stdbool.h>
 
 int main(){
+
+    char productName[50] = "";
+    float productPrice = 0.0f;
+    int amount = 0;
+
+    printf("Please enter the product name:\t");
+    scanf("%s", &productName);
+
+    printf("Please enter the product price:\t");
+    scanf(" %f", &productPrice);
+
+    printf("Please enter the amount of products you want:\t");
+    scanf("%d", &amount);
+
+    printf("You are buying %d amount of %s, which each cost %.2f.\nIt brings your total to: %.2f", amount, productName, productPrice, amount*productPrice);
 
     return 0;
 }
