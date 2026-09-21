00b99046407e4090d942ad2a2d671a0f498f8f94
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 12:51:18 +0330

    Implementing a grade calculator for arrays practice

--- Session_08/main.c
+++ Session_08/main.c
@@ -5,5 +5,14 @@
 
 int main(){
 
+    int numbers[] = {10, 20, 30, 40, 50};
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+
+    for(int i = 4; i > 0; i--){
+        printf("From %d to %d is %c grade.\n", numbers[i-1], numbers[i], grades[4-i]);
+    }
+    printf("From 0 to 10 is F grade.");
+    
+
     return 0;
 }
