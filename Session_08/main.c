2cba806e686e946e99273eac43271a0c08ccadf4
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 12:41:35 +0330

    Initial commit for the structure of the array session

---
+++ Session_08/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+    // array = A fixed-size collection of elements of the same data type (Similar to a variable, but it holds more than 1 value)
+
+
+int main(){
+
+    return 0;
+}
