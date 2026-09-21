17bfc8482a3dc2b9999556518ef0bdbe3e9231d5
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 20:50:24 +0330

    Fixing the bug with the leftovers inside the input buffer with getchar() function and also replacing the newline at the end of the names that gets() function reads by default

--- Session_10/main.c
+++ Session_10/main.c
@@ -4,38 +4,38 @@
 
 
 #include <stdio.h>
+#include <string.h>
 
-void whosOlder(char name1[], char name2[]){
-    for(int i = 0; i < sizeof(name1); i++){
-        printf("%c", name1[i]);
-    }
-    printf(" is older than ");
-    for(int j = 0; j < sizeof(name2); j++){
-        printf("%c", name2[j]);
-    }
-    printf(".\n");
+void whosOlder(char name1[], char name2[]) {
+    printf("%s is older than %s.\n", name1, name2);
 }
 
-int main(){
+int main() {
 
-    char name1[50] = "", name2[50] = "";
+    char name1[50] = "";
+    char name2[50] = "";
     int age1 = 0, age2 = 0;
 
     printf("Please enter the first name:\n");
     fgets(name1, sizeof(name1), stdin);
-    char ch = getchar();
-    printf("Please enter the age of %s.\n", name1);
-    scanf("%d", age1);
-    printf("\n");
+    name1[strcspn(name1, "\n")] = '\0';
 
-    printf("Please enter the second name:\n");
+    printf("Please enter the age of %s:\n", name1);
+    scanf("%d", &age1);
+    getchar();
+
+    printf("\nPlease enter the second name:\n");
     fgets(name2, sizeof(name2), stdin);
-    ch = getchar();
-    printf("Please enter the age of %s.\n", name2);
-    scanf("%d", age2);
+    name2[strcspn(name2, "\n")] = '\0';
+
+    printf("Please enter the age of %s:\n", name2);
+    scanf("%d", &age2);
+
     printf("\n");
 
-    (age1 >= age2)? whosOlder(name1, name2) : whosOlder(name2, name1);
+    (age1 >= age2)
+        ? whosOlder(name1, name2)
+        : whosOlder(name2, name1);
 
     return 0;
 }
