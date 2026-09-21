0e47aa41920769efc5e74d648434729624d73fa1
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 21:41:17 +0330

    Implementing a student struct

--- Session_13/main.c
+++ Session_13/main.c
@@ -2,10 +2,19 @@
 //              It is similar to Objects in other programming languages.
 
 #include <stdio.h>
+#include <stdbool.h>
+struct Student{
+
+    char name[50];
+    float grade;
+    int age;
+    bool isFullTime;
+
+};
 
 int main(){
 
 
-    
+
     return 0;
 }
