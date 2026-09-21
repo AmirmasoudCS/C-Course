c2a591df4af78f1fee2c258d6c80ae56cd356d71
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 21:27:55 +0330

    Initial commit for the session about struct

---
+++ Session_13/main.c
@@ -0,0 +1,11 @@
+// struct =     A custom container that holds multiple pieces of related information.
+//              It is similar to Objects in other programming languages.
+
+#include <stdio.h>
+
+int main(){
+
+
+    
+    return 0;
+}
