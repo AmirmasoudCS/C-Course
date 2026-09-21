f114d202ff1c3f41ece1f732186556f7241192b9
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-21 21:08:43 +0330

    Initial commit for the enums session

---
+++ Session_12/main.c
@@ -0,0 +1,9 @@
+// enums =  A user-defined data type that consists of a set of named integer constants.
+//          benefits: Replaces numbers with readable names.
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
