889d881648b27a0c53dff52fdad280d5b8ada7ee
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:31:40 +0330

    Dynamically allocating space to our array

--- Session_16/main.c
+++ Session_16/main.c
@@ -1,10 +1,15 @@
-// malloc() = A function in C that dynamically allocates a specified number of bytes in memory
+// malloc() = A function in C that dynamically allocates a specified number of bytes in memory. (Memory Allocation)
 
 #include <stdio.h>
 
 int main(){
 
+    int numberOfGrades = 0;
+    printf("Please enter the number of grades you wish to input:\t");
+    scanf("%d", &numberOfGrades);
 
-    
+    int *grades = malloc(numberOfGrades * sizeof(int));      // dynamically allocating memory for the grades array user is going to input.
+
+
     return 0;
 }
