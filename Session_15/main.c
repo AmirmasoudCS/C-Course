23d1c54307b8402691d6c065d6db8ab850223fd4
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:18:22 +0330

    Removing some of the characters that were rendered incorrectly

--- Session_15/main.c
+++ Session_15/main.c
@@ -8,11 +8,11 @@ int main(){
 
     FILE *pFile = fopen("./output.txt", "w");
     if(pFile == NULL){
-        printf("Failed to open the file!\n");
+        printf("Failed to open the file!\n\n");
         return 1;
     }
 
-    char text[] = "“Yes,there are two paths you can go by, but in the long run, there's still time to change the road you're on.”\n― Led Zeppelin\n";
+    char text[] = "Yes,there are two paths you can go by, but in the long run, there's still time to change the road you're on.\n- Led Zeppelin\n";
 
     fprintf(pFile, "%s", text);
     printf("File was successfully written!\n");
