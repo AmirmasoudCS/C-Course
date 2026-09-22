0a23fb0b3c226d63103df7ac2c502a1a0826c7c6
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:54:32 +0330

    Initial commit for the calloc() session

---
+++ Session_17/main.c
@@ -0,0 +1,10 @@
+// calloc() =   Contiguous Allocation.
+//              Allocates memory dynamically and sets all allocated bytes to 0.
+//              malloc() is faster, but calloc() leads to less bugs.
+//              calloc(#, size)
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
