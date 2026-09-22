72cc2b4df9d4dabae5a104f54ecaf2bb884fe746
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 08:50:15 +0330

    Initial commit for the pointers session

---
+++ Session_14/main.c
@@ -0,0 +1,9 @@
+// pointer =    A variable that stores the memory address of another variable.
+//              Benefit: They help avoid wasting memory by allowing you to pass the address of a large data structure instead of copying the entire data.
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
