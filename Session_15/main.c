7f66de4873096d403e9e5510336cc994e9222251
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:05:43 +0330

    Opening a file in write mode

--- Session_15/main.c
+++ Session_15/main.c
@@ -4,5 +4,17 @@
 
 int main(){
 
+
+    FILE *pFile = fopen("./output.txt", "w");
+    if(pFile == NULL){
+        printf("Failed to open the file!\n");
+        return 1;
+    }
+
+
+
+    fclose(pFile);
+
+
     return 0;
 }
