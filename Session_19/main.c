189efbdec5acaf19b785f52cd08152d15b1b8142
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 10:59:01 +0330

    Creating a digital clock project

---
+++ Session_19/main.c
@@ -0,0 +1,27 @@
+// Digital Clock
+
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <windows.h>
+
+int main(){
+
+    time_t rawtime = 0; // Jan 1 1970 (Epoch)
+    struct tm *pTime = NULL;
+    bool isRunning = true;
+
+    printf("========== Digital Clock ==========\n");
+    while(isRunning){
+        Sleep(1000);
+        time(&rawtime);
+        pTime = localtime(&rawtime);
+        printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+
+
+    }
+
+
+
+    return 0;
+}
