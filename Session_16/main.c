b331374baa9307d0e7df8a33b0dc6a45fde4dd6b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:34:44 +0330

    checking for failed memory allocation

--- Session_16/main.c
+++ Session_16/main.c
@@ -1,6 +1,7 @@
 // malloc() = A function in C that dynamically allocates a specified number of bytes in memory. (Memory Allocation)
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
@@ -9,7 +10,14 @@ int main(){
     scanf("%d", &numberOfGrades);
 
     int *grades = malloc(numberOfGrades * sizeof(int));      // dynamically allocating memory for the grades array user is going to input.
+    if(grades == NULL){
+        printf("Failed to allocate memory!\n");
+        return 1;
+    }
 
+
+    free(grades);                                           // freeing up the space we dynamically allocated.
+    grades = NULL;                                          // avoiding having `dangling pointer`
 
     return 0;
 }
