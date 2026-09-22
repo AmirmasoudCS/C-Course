47ccfac129f9912862f15493d8874fc06f7f2af3
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 09:59:28 +0330

    Implementing the malloc() to see its behavior

--- Session_17/main.c
+++ Session_17/main.c
@@ -2,9 +2,33 @@
 //              Allocates memory dynamically and sets all allocated bytes to 0.
 //              malloc() is faster, but calloc() leads to less bugs.
 //              calloc(#, size)
+
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
+
+    int numberOfPlayers = 0;
+    printf("Please enter the number of players:\t");
+    scanf("%d", &numberOfPlayers);
+    printf("\n");
+
+    int *scores = malloc(numberOfPlayers * (sizeof(int)));
+    if(scores == NULL){
+        printf("Failed to allocate memory to scores!\n");
+        return 1;
+    }
+    for(int i = 0; i < numberOfPlayers; i++){
+        printf("%d, ", &scores[i]);
+    }
+
+
+    free(scores);
+    scores = NULL;
+
+
+
 
     return 0;
 }
