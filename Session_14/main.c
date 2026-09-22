555d1d4e1671c5445c496f9180088f94ccd21469
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-22 08:59:36 +0330

    Passing by reference using a pointer

--- Session_14/main.c
+++ Session_14/main.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+void birthday(int *age);
+
 int main(){
 
     int age = 25;
@@ -12,7 +14,16 @@ int main(){
 
     int *pAge = &age;                           // `*` is also known as the dereference operator, and `&` gives you the address/reference of a variable in the memory.
 
-    printf("%p", pAge);
+    printf("%p\n\n", pAge);
+
+    printf("The age before incrementing: %d\n", age);
+    birthday(pAge);
+    printf("The age after incrementing: %d\n", age);
+
 
     return 0;
+}
+
+void birthday(int *age){
+    (*age)++;
 }
